9fd06689ba0663e728750662ca699d6f74aae529
t3hcyberrat <emil.pestean@gmail.com>
2020-08-30 23:01:11 +0300

    Implemented some cases

--- gormseeder.go
+++ gormseeder.go
@@ -5,12 +5,11 @@ import (
 	"reflect"
 
 	"github.com/bxcodec/faker"
-
 	_ "github.com/jinzhu/gorm/dialects/mysql"
 	L "github.com/zkynetio/logger"
 )
 
-type ModelWrapper struct{}
+const tagName string = "gormseeder"
 
 // Init ...
 func Init(dialect string, connectionString string, databaseName string) {
@@ -34,32 +33,94 @@ func Run(Model interface{}) {
 
 func generateFields(Model interface{}) {
 
-	// TODO ... FILL THE MODEL...
-	composedStruct := CreateObject(Model)
+	modelMap := CreateObject(Model)
 
-	for i, v := range composedStruct {
-		fmt.Println(i, v)
-		switch v.(type) {
-		case string:
-			composedStruct[i] = faker.Email()
+	for k, v := range modelMap {
+
+		switch k {
+		case "lat":
+			modelMap[k] = faker.Latitude()
+		case "long":
+			modelMap[k] = faker.Longitude()
+		case "cc_number":
+			modelMap[k] = faker.CCNumber()
+		case "cc_type":
+			modelMap[k] = faker.CCType()
+		case "email":
+			modelMap[k] = faker.Email()
+		case "domain_name":
+			modelMap[k] = faker.DomainName()
+		case "ipv4":
+			modelMap[k] = faker.IPv4()
+		case "ipv6":
+			modelMap[k] = faker.IPv6()
+		case "password":
+			modelMap[k] = faker.Password()
+		case "jwt":
+			modelMap[k] = faker.Jwt()
+		case "phone_number":
+			modelMap[k] = faker.Phonenumber()
+		case "mac_address":
+			modelMap[k] = faker.MacAddress()
+		case "url":
+			modelMap[k] = faker.URL()
+		case "username":
+			modelMap[k] = faker.Username()
+		case "title_male":
+			modelMap[k] = faker.TitleMale()
+		case "title_female":
+			modelMap[k] = faker.TitleFemale()
+		case "first_name":
+			modelMap[k] = faker.FirstName()
+		case "first_name_male":
+			modelMap[k] = faker.FirstNameMale()
+		case "first_name_female":
+			modelMap[k] = faker.FirstNameFemale()
+		case "last_name":
+			modelMap[k] = faker.LastName()
+		case "name":
+			modelMap[k] = faker.Name()
+		case "word":
+			modelMap[k] = faker.Word()
+		case "sentence":
+			modelMap[k] = faker.Sentence()
+		case "paragraph":
+			modelMap[k] = faker.Paragraph()
+		case "currency":
+			modelMap[k] = faker.Currency()
+		case "amount_with_currency":
+			modelMap[k] = faker.AmountWithCurrency()
+		case "uuid_hyphenated":
+			modelMap[k] = faker.UUIDHyphenated()
+		case "uuid_digit":
+			modelMap[k] = faker.UUIDDigit()
 		}
+
 	}
 
-	fmt.Println(composedStruct)
+	fmt.Println(modelMap)
 
 }
 
 // CreateObject ...
-func CreateObject(Model interface{}) []interface{} {
+func CreateObject(Model interface{}) map[string]interface{} {
 
 	modelValues := reflect.ValueOf(Model)
-	ret := make([]interface{}, modelValues.NumField())
+	result := make(map[string]interface{})
 
 	for i := 0; i < modelValues.NumField(); i++ {
-		ret[i] = modelValues.Field(i).Interface()
+
+		tag := modelValues.Type().Field(i).Tag.Get(tagName)
+
+		if tag == "" {
+			tag = modelValues.Type().Field(i).Name
+		}
+
+		result[tag] = modelValues.Field(i).Interface()
+
 	}
 
-	return ret
+	return result
 
 }
 
--- testing/main.go
+++ testing/main.go
@@ -7,8 +7,34 @@ import (
 
 // Person ...
 type Person struct {
-	FirstName int
-	LastName  string
+	Latitude           float32 `gormseeder:"lat"`
+	Longitude          float32 `gormseeder:"long"`
+	CreditCardNumber   string  `gormseeder:"cc_number"`
+	CreditCardType     string  `gormseeder:"cc_type"`
+	Email              string  `gormseeder:"email"`
+	DomainName         string  `gormseeder:"domain_name"`
+	IPV4               string  `gormseeder:"ipv4"`
+	IPV6               string  `gormseeder:"ipv6"`
+	Password           string  `gormseeder:"password"`
+	Jwt                string  `gormseeder:"jwt"`
+	PhoneNumber        string  `gormseeder:"phone_number"`
+	MacAddress         string  `gormseeder:"mac_address"`
+	URL                string  `gormseeder:"url"`
+	UserName           string  `gormseeder:"username"`
+	TitleMale          string  `gormseeder:"title_male"`
+	TitleFemale        string  `gormseeder:"title_female"`
+	FirstName          string  `gormseeder:"first_name"`
+	FirstNameMale      string  `gormseeder:"first_name_male"`
+	FirstNameFemale    string  `gormseeder:"first_name_female"`
+	LastName           string  `gormseeder:"last_name"`
+	Name               string  `gormseeder:"name"`
+	Word               string  `gormseeder:"word"`
+	Sentence           string  `gormseeder:"sentence"`
+	Paragraph          string  `gormseeder:"paragraph"`
+	Currency           string  `gormseeder:"currency"`
+	AmountWithCurrency string  `gormseeder:"amount_with_currency"`
+	UUIDHypenated      string  `gormseeder:"uuid_hyphenated"`
+	UUID               string  `gormseeder:"uuid_digit"`
 	gorm.Model
 }
 
