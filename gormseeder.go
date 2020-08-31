aed88fefbc0eaf4395a7e1ed66e33a6989f312f7
zkynet <anon@zkynet.io>
2020-08-31 11:30:49 +0000

    Refactoring code to use faker for injections and moving dbmodule outside of code.

--- gormseeder.go
+++ gormseeder.go
@@ -5,139 +5,70 @@ import (
 	"reflect"
 
 	"github.com/bxcodec/faker"
+	c "github.com/fatih/color"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
-	L "github.com/zkynetio/logger"
+	gormwrapper "github.com/zkynet/gorm-wrapper"
+	"github.com/zkynetio/logger"
 )
 
 const tagName string = "gormseeder"
 
+var dbTag string
+
 // Init ...
-func Init(dialect string, connectionString string, databaseName string) {
-
-	Connect(dialect, connectionString, databaseName)
-	Connection.LogMode(false)
-	Connection.SetLogger(&L.GORMLogger{})
-
+func Init(dialect string, connectionString string, databaseName string, DBTag string, sqlLogging bool) error {
+	err, _ := logger.Init(&logger.LoggingConfig{
+		DefaultLogTag:   "testing-logs",
+		DefaultLogLevel: logger.LogLevelInfo,
+		WithTrace:       true,
+		SimpleTrace:     true,
+		PrettyPrint:     true,
+		Colors:          true,
+		Type:            "stdout",
+	})
+	if err != nil {
+		return err
+	}
+	dbTag = DBTag
+	connectErr := gormwrapper.Connect(dialect, connectionString, dbTag)
+	if connectErr != nil {
+		connectErr.Log()
+		return connectErr
+	}
+	gormwrapper.ConnectionMap[dbTag].LogMode(sqlLogging)
+	if sqlLogging {
+		gormwrapper.ConnectionMap[dbTag].SetLogger(&logger.GORMLogger{})
+	}
+	return nil
 }
 
 // Run ...
-func Run(Model interface{}) {
+func Run(Models ...interface{}) {
 
-	// Generate rand data
-	modelMap := generateFields(Model)
-
-	// Inject field to DB with rand data
-	SaveModelToDatabase(Model, modelMap)
-
-}
-
-func generateFields(Model interface{}) map[string]interface{} {
-
-	modelMap := CreateObject(Model)
-
-	for k := range modelMap {
-
-		switch k {
-		case "lat":
-			modelMap[k] = faker.Latitude()
-		case "long":
-			modelMap[k] = faker.Longitude()
-		case "cc_number":
-			modelMap[k] = faker.CCNumber()
-		case "cc_type":
-			modelMap[k] = faker.CCType()
-		case "email":
-			modelMap[k] = faker.Email()
-		case "domain_name":
-			modelMap[k] = faker.DomainName()
-		case "ipv4":
-			modelMap[k] = faker.IPv4()
-		case "ipv6":
-			modelMap[k] = faker.IPv6()
-		case "password":
-			modelMap[k] = faker.Password()
-		case "jwt":
-			modelMap[k] = faker.Jwt()
-		case "phone_number":
-			modelMap[k] = faker.Phonenumber()
-		case "mac_address":
-			modelMap[k] = faker.MacAddress()
-		case "url":
-			modelMap[k] = faker.URL()
-		case "username":
-			modelMap[k] = faker.Username()
-		case "title_male":
-			modelMap[k] = faker.TitleMale()
-		case "title_female":
-			modelMap[k] = faker.TitleFemale()
-		case "first_name":
-			modelMap[k] = faker.FirstName()
-		case "first_name_male":
-			modelMap[k] = faker.FirstNameMale()
-		case "first_name_female":
-			modelMap[k] = faker.FirstNameFemale()
-		case "last_name":
-			modelMap[k] = faker.LastName()
-		case "name":
-			modelMap[k] = faker.Name()
-		case "word":
-			modelMap[k] = faker.Word()
-		case "sentence":
-			modelMap[k] = faker.Sentence()
-		case "paragraph":
-			modelMap[k] = faker.Paragraph()
-		case "currency":
-			modelMap[k] = faker.Currency()
-		case "amount_with_currency":
-			modelMap[k] = faker.AmountWithCurrency()
-		case "uuid_hyphenated":
-			modelMap[k] = faker.UUIDHyphenated()
-		case "uuid_digit":
-			modelMap[k] = faker.UUIDDigit()
+	for i := range Models {
+		err := faker.FakeData(&Models[i])
+		if err != nil {
+			fmt.Println(err)
 		}
-
+		SaveModelToDatabase(Models[i])
+		c.Green("Create Type: " + reflect.TypeOf(Models[i]).String())
 	}
-
-	return modelMap
-
-}
-
-// CreateObject ...
-func CreateObject(Model interface{}) map[string]interface{} {
-
-	modelValues := reflect.ValueOf(Model)
-	result := make(map[string]interface{})
-
-	for i := 0; i < modelValues.NumField(); i++ {
-
-		tag := modelValues.Type().Field(i).Tag.Get(tagName)
-
-		if tag == "" {
-			tag = modelValues.Type().Field(i).Name
-		}
-
-		result[tag] = modelValues.Field(i).Interface()
-
-	}
-
-	return result
 
 }
 
 // SaveModelToDatabase ...
-func SaveModelToDatabase(M interface{}, modelMap map[string]interface{}) error {
+func SaveModelToDatabase(M interface{}) error {
+	defer func() {
+		if r := recover(); r != nil {
+			logger.GenericError(logger.GetRecoverError(r)).Log()
+		}
+	}()
 
-	x := reflect.TypeOf(&M)
-	// y := reflect.Indirect(&x)
-
-	fmt.Println(x)
-	// fmt.Println(y)
-
-	// err := CreateFromMap(&x, modelMap)
-	// if err != nil {
-	// 	err.Log()
-	// 	return err
-	// }
+	err := gormwrapper.Create(dbTag, &M)
+	if err != nil {
+		err.Log()
+		return err
+	}
 
 	return nil
 
--- testing/main.go
+++ testing/main.go
@@ -1,45 +1,57 @@
 package main
 
 import (
+	"os"
+
 	gormseeder "github.com/t3hcyberrat/gorm-seeder"
+	gormwrapper "github.com/zkynet/gorm-wrapper"
 )
 
 // Person ...
 type Person struct {
-	Latitude           float32 `gormseeder:"lat"`
-	Longitude          float32 `gormseeder:"long"`
-	CreditCardNumber   string  `gormseeder:"cc_number"`
-	CreditCardType     string  `gormseeder:"cc_type"`
-	Email              string  `gormseeder:"email"`
-	DomainName         string  `gormseeder:"domain_name"`
-	IPV4               string  `gormseeder:"ipv4"`
-	IPV6               string  `gormseeder:"ipv6"`
-	Password           string  `gormseeder:"password"`
-	Jwt                string  `gormseeder:"jwt"`
-	PhoneNumber        string  `gormseeder:"phone_number"`
-	MacAddress         string  `gormseeder:"mac_address"`
-	URL                string  `gormseeder:"url"`
-	UserName           string  `gormseeder:"username"`
-	TitleMale          string  `gormseeder:"title_male"`
-	TitleFemale        string  `gormseeder:"title_female"`
-	FirstName          string  `gormseeder:"first_name"`
-	FirstNameMale      string  `gormseeder:"first_name_male"`
-	FirstNameFemale    string  `gormseeder:"first_name_female"`
-	LastName           string  `gormseeder:"last_name"`
-	Name               string  `gormseeder:"name"`
-	Word               string  `gormseeder:"word"`
-	Sentence           string  `gormseeder:"sentence"`
-	Paragraph          string  `gormseeder:"paragraph"`
-	Currency           string  `gormseeder:"currency"`
-	AmountWithCurrency string  `gormseeder:"amount_with_currency"`
-	UUIDHypenated      string  `gormseeder:"uuid_hyphenated"`
-	UUID               string  `gormseeder:"uuid_digit"`
-	gormseeder.BaseModel
+	Latitude           float32 `faker:"lat"`
+	Longitude          float32 `faker:"long"`
+	CreditCardNumber   string  `faker:"cc_number"`
+	CreditCardType     string  `faker:"cc_type"`
+	Email              string  `faker:"email"`
+	DomainName         string  `faker:"domain_name"`
+	IPV4               string  `faker:"ipv4"`
+	IPV6               string  `faker:"ipv6"`
+	Password           string  `faker:"password"`
+	Jwt                string  `faker:"jwt"`
+	PhoneNumber        string  `faker:"phone_number"`
+	MacAddress         string  `faker:"mac_address"`
+	URL                string  `faker:"url"`
+	UserName           string  `faker:"username"`
+	TitleMale          string  `faker:"title_male"`
+	TitleFemale        string  `faker:"title_female"`
+	FirstName          string  `faker:"first_name"`
+	FirstNameMale      string  `faker:"first_name_male"`
+	FirstNameFemale    string  `faker:"first_name_female"`
+	LastName           string  `faker:"last_name"`
+	Name               string  `faker:"name"`
+	Word               string  `faker:"word"`
+	Sentence           string  `faker:"sentence"`
+	Paragraph          string  `faker:"paragraph"`
+	Currency           string  `faker:"currency"`
+	AmountWithCurrency string  `faker:"amount_with_currency"`
+	UUIDHypenated      string  `faker:"uuid_hyphenated"`
+	UUID               string  `faker:"uuid_digit"`
+	gormwrapper.BaseModel
+}
+type XXX struct {
+	Name string
 }
 
 func main() {
 
-	gormseeder.Init("mysql", "root:hacktheplanet@/", "nethkr")
-	gormseeder.Run(Person{})
+	err := gormseeder.Init("mysql", "root:hacktheplanet@/", "nethkr", "seeder-connection", true)
+	if err != nil {
+		os.Exit(1)
+	}
+	gormseeder.Run(
+		Person{},
+		XXX{},
+	)
 
 }
